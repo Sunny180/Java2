7cd19234b1630d34babe56d8bc49d7ca6caa4585
user <user@es301n01>
2019-03-05 11:08:41 +0800

    abc

---
+++ com.tom/src/com/tom/Abc.java
@@ -0,0 +1,24 @@
+package com.tom;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> bag = new ArrayList<>();
+		bag.add(3);
+		bag.add(88);
+		bag.add(3);
+		bag.add(94);
+			
+		Set<Integer> food = new HashSet<>();
+		food.add(3);
+		food.add(88);
+		food.add(3);
+	}
+	
+}
