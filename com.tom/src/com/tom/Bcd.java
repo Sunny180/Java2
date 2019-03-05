e1b2499b74b595c786ea25e5e51143bb41f8408b
user <user@es301n01>
2019-03-05 11:47:23 +0800

    bcd

---
+++ com.tom/src/com/tom/Bcd.java
@@ -0,0 +1,24 @@
+package com.tom;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Bcd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> read = new ArrayList<>();
+		read.add("小偷家族");
+		read.add("達文西傳");
+		read.add("小偷家族");
+	
+			
+		Set<Integer> students = new HashSet<>();
+		students.add(407570256);
+		students.add(407570305);
+		students.add(407570333);		
+	}
+
+}
