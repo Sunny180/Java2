172f25ad1be6ce233a254f23e4f538d3b55c0525
user <user@es301n14>
2019-05-07 12:00:31 +0800

    456

--- com.tom/src/com/tom/Tester.java
+++ com.tom/src/com/tom/Tester.java
@@ -8,14 +8,37 @@ public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = Integer.parseInt("35");
-		File file = new File("aa.txt");
+		for(int i=0; i++; i<=2000000) {
+			
+		}
+		File dir = new File("c:\\upfile");
+		if(dir.exists()&& dir.isDirectory()) {
+			File[] files = dir.listFiles();
+			for(File f :files) {
+				if(f.isFile()) {
+				System.out.println(f.getName());
+				}
+			}
+			System.out.println(dir.mkdir());
+			File dd = new File(dir,"aaa\\bb\\c");
+			System.out.println(dd.mkdirs());
+			System.out.println(dd.delete());
+		}
+/*		File dir = new File("c:\\upfile");
+		if(dir.exists()&& dir.isDirectory()) {
+			String[] files = dir.list();
+			for(String s :files) {
+				System.out.println(s);
+			}
+		}*/
+//		int n = Integer.parseInt("35");
+/*		File file = new File("aa.txt");
 		System.out.println(file.exists());
 		System.out.println(file.canWrite());
 		System.out.println(file.isHidden());
 		System.out.println(file.length());
 		System.out.println(file.getName());
-		
+		System.out.println(file.getAbsolutePath());*/
 		
 /*		try {
 			FileReader fr = new FileReader("aa.txt");
